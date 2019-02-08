268f5e4bdbb623c2d7dd1c3ef82046a85fe13f48
Sylvain Pont <pont.sylvain@gmail.com>
2019-02-08 11:34:00 +0100

    Update theme to use memoizeOne and have common colors

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import memoizeOne from 'memoize-one';
 
 // Material UI
 // Normalize CSS styles between borwsers and OS
@@ -9,8 +10,7 @@ import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 // https://github.com/typekit/webfontloader
 import WebFontLoader from 'webfontloader';
 
-let theme;
-let fontFamily = '';
+let fontFamily = null;
 
 /**
  * Function to set the Material-UI lib theme
@@ -20,11 +20,7 @@ let fontFamily = '';
  * @return  {object}  muiTheme
  * @author Sylvain Pont
  */
-const getTheme = () => {
-  if (!_.isUndefined(theme)) {
-    return theme;
-  }
-
+const getTheme = memoizeOne(theme => {
   // Roboto always here as backup font
   const fonts = {
     google: {
@@ -35,44 +31,55 @@ const getTheme = () => {
 
   fontFamily = 'Roboto, Helvetica, Arial, sans-serif';
 
-  // const hardWhite = 'rgb(255, 255, 255)';
-  // const mediumWhite = 'rgb(250, 250, 250)';
-  // const lightWhite = 'rgb(245, 245, 245)';
-  theme = createMuiTheme({
-    // palette: {
-    //   primary: {
-    //     // light and dark entries, if not set, will be generated
-    //     main: 'rgb(203, 0, 68)',
-    //     contrastText: mediumWhite,
-    //   },
-    //   secondary: {
-    //     // light and dark entries, if not set, will be generated
-    //     main: 'rgb(62, 61, 64)',
-    //     contrastText: mediumWhite,
-    //   },
-    //   common: {
-    //     white: mediumWhite,
-    //     black: 'rgb(22, 22, 22)',
-    //     yammerblue: 'rgb(0, 136, 206)',
-    //   },
-    //   background: {
-    //     default: mediumWhite,
-    //     paper: lightWhite,
-    //     sncfgrey: 'rgb(224, 225, 221)',
-    //   },
-    // },
-    typography: { fontFamily, useNextVariants: true },
-  });
-  theme.breakpoints.getName = (width, { keys, values } = theme.breakpoints) =>
-    _.findLast(keys, key => values[key] <= width);
+  const hardWhite = 'rgb(255, 255, 255)';
+  const mediumWhite = 'rgb(250, 250, 250)';
+  const lightWhite = 'rgb(245, 245, 245)';
+  const muiTheme = createMuiTheme(
+    _.merge(
+      {
+        palette: {
+          // primary: {
+          //   // light and dark entries, if not set, will be generated
+          //   main: 'rgb(203, 0, 68)',
+          //   contrastText: mediumWhite,
+          // },
+          // secondary: {
+          //   // light and dark entries, if not set, will be generated
+          //   main: 'rgb(62, 61, 64)',
+          //   contrastText: mediumWhite,
+          // },
+          common: {
+            lightWhite,
+            mediumWhite,
+            white: mediumWhite,
+            hardWhite,
+            black: 'rgb(22, 22, 22)',
+            yammerblue: 'rgb(0, 136, 206)',
+          },
+          // "Whiteish" color
+          background: {
+            default: mediumWhite,
+            paper: lightWhite,
+            // sncfgrey: 'rgb(224, 225, 221)',
+          },
+        },
+        typography: { fontFamily, useNextVariants: true },
+      },
+      theme,
+    ),
+  );
+  muiTheme.breakpoints.getName = (
+    width,
+    { keys, values } = muiTheme.breakpoints,
+  ) => _.findLast(keys, key => values[key] <= width);
 
-  window.MUI_THEME = theme;
+  window.MUI_THEME = muiTheme;
 
-  return theme;
-};
+  return muiTheme;
+});
 
 const getFontFamily = () => {
-  if (_.isUndefined(theme)) {
+  if (!fontFamily) {
     getTheme();
   }
   return fontFamily;
